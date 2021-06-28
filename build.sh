105e0e399f6dc893405bc874cb566388b4e0cb26
nexware <wej22007@outlook.com>
2021-06-28 14:40:12 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# build.sh - contains main build script
+# Distributed with NexNix, licensed under the MIT license
+# See LICENSE
---
+++ nexboot/build.sh
@@ -0,0 +1,4 @@
+# !/bin/sh
+# build.sh - contains build script for nexboot
+# Distributed with NexNix, licensed under the MIT license
+# See LICENSE
