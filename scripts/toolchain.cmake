f448a7e259ac19125d5a835daa65b4df6beac1ae
nexos-dev <wej22007@outlook.com>
2021-08-12 11:34:00 -0400

    Relicense to Apache 2.0

--- scripts/toolchain.cmake
+++ scripts/toolchain.cmake
@@ -1,6 +1,16 @@
 # toolchain.cmake - contains toolchain definitions
-# Distributed with NexNix, licensed under the MIT license
-# See LICENSE
+# Copyright 2021 Jedidiah Thompson
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Set up compiler definitions
 set(CMAKE_C_COMPILER ${GLOBAL_CROSS}/bin/${GLOBAL_MACH}-elf-gcc)
